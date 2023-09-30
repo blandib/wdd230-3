5cdd3cce6a622582525c03b21cba6ff130eeb886
Blandine <blandinebukasa118@gmail.com>
2023-09-30 14:00:23 +0200

    Update

--- index/script/getDate.js
+++ index/script/getDate.js
@@ -4,3 +4,34 @@ const currentYear = new Date();document.getElementById("year").innerHTML = curre
 
 let date = document.lastModified;
 document.getElementById("lastModified").innerHTML = "Last motifified on :" + date;
+
+//humburger
+const button = document.querySelector(".side-nav")
+const menu =  document.querySelector("#burger-menu")
+
+menu.addEventListener("click", () => {
+	button.classList.toggle("open");
+	menu.classList.toggle("open");
+});
+
+
+
+
+
+
+
+
+
+/*function myFunction(x) {
+	x.classList.toggle("change");
+  }
+  
+  function openSlideMenu(){
+	document.getElementById('burger-menu').style.width = '100%';
+  }
+  
+  function closeSlideMenu(){
+	document.getElementById('burger-menu').style.width = '0px';
+  }*/
+  
+  
