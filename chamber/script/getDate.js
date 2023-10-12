4c6b5f4e58e82493ef19395cc53a3f5cd3b296b7
Blandine <blandinebukasa118@gmail.com>
2023-10-12 14:29:37 +0200

    Update

--- chamber/script/getDate.js
+++ chamber/script/getDate.js
@@ -4,3 +4,27 @@ const currentYear = new Date();document.getElementById("year").innerHTML = curre
 
 let date = document.lastModified;
 document.getElementById("lastModified").innerHTML = "Last motifified on :" + date;
+//humburger
+const button = document.querySelector("#side-nav")
+const menu =  document.querySelector(".menu")
+
+menu.addEventListener("click", () => {
+	button.classList.toggle("open");
+	menu.classList.toggle("open");
+});
+
+//mode
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector(".content");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🌑")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🌑";
+	}
+});
