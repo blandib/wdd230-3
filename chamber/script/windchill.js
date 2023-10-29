7e82c8990243bd213d68492f7237d5ffde14548e
Blandine <blandinebukasa118@gmail.com>
2023-10-29 21:44:50 +0200

    Update

--- chamber/script/windchill.js
+++ chamber/script/windchill.js
@@ -58,3 +58,60 @@ if (temp <= tempLimit && wind > windLimit) {
 }
 }
 }
+
+// 1️⃣ Initialize display element variable
+/*const visitsDisplay = document.querySelector(".visits");
+
+// 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+// 3️⃣ Determine if this is the first visit or display the number of visits. We wrote this example backwards in order for you to think deeply about the logic.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+  visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
+} else if(numVisits !==1) {
+  visitsDisplay.textContent = `Back so soon! Awesome!`;
+	//visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}else {
+  visitsDisplay.textContent = `You last visited n days ago`
+}
+
+// 4️⃣ increment the number of visits by one.
+numVisits++;
+
+// 5️⃣ store the new visit total into localStorage, key=numVisits-ls
+localStorage.setItem("numVisits-ls", numVisits);
+
+// 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.*/
+// Get the current date and time
+ 
+//localstore
+// Save the current date to localStorage
+localStorage.setItem('lastVisit', new Date().getTime());
+
+// Retrieve the last visit date from localStorage
+const lastVisit = localStorage.getItem('lastVisit');
+// Get the current date and time
+const currentDate = new Date();
+
+// Get the last visit date and time from localStorage
+const lastVisitDate = new Date(parseInt(localStorage.getItem('lastVisit')));
+
+// Calculate the difference between the two dates in milliseconds
+const differenceInMilliseconds = currentDate - lastVisitDate;
+
+// Calculate the difference between the two dates in days
+const differenceInDays = Math.floor(differenceInMilliseconds / (1000 * 60 * 60 * 24));
+
+// Get a reference to the sidebar content area
+const sidebarContentArea = document.querySelector('.sidebar-content-area');
+
+// Check if this is the user's first visit
+if (lastVisit === 0) {
+  sidebarContentArea.textContent = `Welcome! Let us know if you have any questions`;
+} else if (differenceInDays === 1) {
+  sidebarContentArea.textContent = `Back so soon! Awesome!`;
+} else {
+  const message = `You last visited ${differenceInDays} ${differenceInDays !== 2 ? 'day' : 'days'} ago.`;
+  //sidebarContentArea.textContent = message;
+}
