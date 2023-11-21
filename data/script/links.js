2530a58de8adca65ae4d32e5ac776b6075f8ea9e
Blandine <blandinebukasa118@gmail.com>
2023-11-21 17:54:13 +0200

    Update

--- data/script/links.js
+++ data/script/links.js
@@ -6,3 +6,31 @@ async function getLinks() {
     console.log(data);
     displayLinks(data);
   }
+  
+  //getLinks();
+
+  const displayLinks = (weeks) => {
+    const cards = document.querySelector('div.cards');
+  
+    weeks.forEach((week) => {
+      let card = document.createElement('section');
+      let h2 = document.createElement('h2');
+      let Links = document.createElement('a');
+      let url = document.createElement('a');
+      let title = document.createElement('h3');
+  
+      h2.textContent = `${week.week} ${week.Links}`;
+  
+      Links.textContent = `Lesson: ${week.Links}`
+      url.textContent = `Lesson: ${week.Links}`
+  
+      card.appendChild(h2);
+      card.appendChild(Links);
+      card.appendChild(url);
+      card.appendChild(title);
+  
+      cards.appendChild(card);
+    });
+  }
+  getLinks();
+ 
