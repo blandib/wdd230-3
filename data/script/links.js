b2265493da89738a6d394cbd0965c1d9097100df
Blandine <blandinebukasa118@gmail.com>
2023-11-21 16:06:44 +0200

    Update

---
+++ data/script/links.js
@@ -0,0 +1,8 @@
+const baseURL = "https://blandib.github.io/wdd230-3/";
+const linksURL = "https://blandib.github.io/wdd230-3/data/links.json";
+async function getLinks() {
+    const response = await fetch(linksURL);
+    const data = await response.json();
+    console.log(data);
+    displayLinks(data);
+  }
